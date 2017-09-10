5e153333c8bbe006fdd472ed5978039076bd3c1c
Chris Nady <nady.christopher@gmail.com>
2017-09-10 19:22:44 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    matches = []
+    array.each do |word|
+      compare_string = word.split("").sort
+      compare_word = word.split("").sort
+      if compare_word == compare_string
+        matches.push(word)
+      end
+    end
+    matches
+  end
+
+end
